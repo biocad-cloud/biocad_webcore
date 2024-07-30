2c1805e810023444e91ceec005cbb113e538bb26
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-07-30 10:10:33 +0800

    Create enrich.php

---
+++ src/phpmath/gcmodeller/enrich.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * enrichment based on gcmodeller background model
+ * 
+ * @param string[] $idset a array of the id for run the enrichment.
+ * @param string $model usually be the file path to the background model xml file.
+ * 
+ * @return array the enrichment result for each term that inside
+ *    the given model file.
+*/
+function enrich($idset, $model) {
+
+}
